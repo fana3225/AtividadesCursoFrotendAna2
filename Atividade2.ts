98ddfb3857eb76e4f68666cc322f2d6bc59b9f09
fana3225 <acaca2234@gmail.com>
2025-11-24 14:03:29 -0300

    Add Atividade2.ts to demonstrate variable types

---
+++ Atividade2.ts
@@ -0,0 +1,7 @@
+let nota: number | string;
+
+nota = 10; // número inteiro
+console.log(`Valor: ${nota}, Tipo: ${typeof nota}`);
+
+nota = "Excelente"; // string textual
+console.log(`Valor: ${nota}, Tipo: ${typeof nota}`);
